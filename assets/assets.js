511357aa8c9ddf8524215b6947d4559f86a32507
ps2223 <120116023+ps2223@users.noreply.github.com>
2025-09-27 23:18:13 +0530

    Add files via upload

---
+++ assets/assets.js
@@ -0,0 +1,17 @@
+import Logo from './Logo.jpg'
+import nigga1 from './nigga1.jpg'
+import Dropdown from './Dropdown.png'
+import turf1 from './turf1.jpeg'
+import turf2 from './turf2.jpeg'
+import turf3 from './turf3.jpeg'
+import turf4 from './turf4.jpeg'
+
+export const assets = {
+    Logo,
+    nigga1,
+    Dropdown,
+    turf1,
+    turf2,
+    turf3,
+    turf4,
+}
